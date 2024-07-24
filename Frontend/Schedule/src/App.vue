44489c4ed4988b30d8e0e2ffba1162a3c138bcc3
Roor Li <1563856124@qq.com>
2024-07-24 15:51:10 +0800

    Basic frontend

--- Frontend/Schedule/src/App.vue
+++ Frontend/Schedule/src/App.vue
@@ -1,14 +1,65 @@
 <script setup>
+import { ref, watch, onMounted } from 'vue';
+import { getTodoList } from '@/utils/testBackend'
+
+const selectedDate = ref(new Date())
+
+// 获取数据
+const todoList = ref([])
+// todoList.value.push(...["11", "22", "33"])
+
+const newTodo = defineModel()
+
+const addTodo = () => {
+  if (!newTodo.value) return
+  todoList.value.push(newTodo.value)
+  newTodo.value = ""
+}
+const deleteTodo = (index) => {
+  // console.log(index);
+  todoList.value.splice(index, 1)
+}
+
+watch(selectedDate, () => {
+  // console.log(getTodoList(selectedDate.value));
+  todoList.value = getTodoList(selectedDate.value)
+})
+
+onMounted(() => {
+  // console.log(selectedDate.value);
+  todoList.value = getTodoList(selectedDate.value)
+  
+})
 
 </script>
 
 <template>
-  <el-calendar/>
-  <div>
-    Here is Schedual
+  <el-calendar v-model="selectedDate">
+
+  </el-calendar>
+  <div class="todo-list">
+    <header>
+      Here is Schedual
+    </header>
+    <input type="text" class="todo-input" v-model="newTodo">
+    <input type="submit" @click="addTodo">
+    <ul>
+      <li v-for="(todo, index) in todoList" :key="index">
+        <input type="checkbox" class="todo-item">{{ todo }}
+        <button @click="deleteTodo(index)">删除</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
-
+.todo-list {
+  flex: 0 1 100%;
+}
+.todo-input {
+  display: inline-block;
+}
+.todo-item {
+  display: inline-block;
+}
 </style>
